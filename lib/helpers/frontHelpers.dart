a3337646c9b5b4879b8389ebc758d330161a2662
kn237 <kanamboma@gmail.com>
2023-06-19 09:20:25 +0100

    first commit

---
+++ lib/helpers/frontHelpers.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class FrontHelpers {
+  final h1 = const TextStyle(fontSize: 20, fontWeight: FontWeight.w900,color: Color(0xff000000));
+
+  late var h2 = h1.copyWith(fontSize: 18);
+
+  late var h3 = h1.copyWith(fontSize: 16);
+
+  late var h4 = h1.copyWith(fontSize: 14);
+
+  late var bodyText = h1.copyWith(fontSize: 12);
+
+  late var noir = const Color(0xff000000);
+
+  late var blanc = const Color(0xfffafafa);
+
+  late var gris = const Color(0xff737373);
+
+  late var orange = const Color(0xffff931e);
+
+  late var orangeGradient = const LinearGradient(
+    colors: [Color(0xffff931e), Color(0xabff931e)],
+    stops: [0, 1],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  late var rouge = const Color(0xffda1421);
+
+  late var rougeGradient = const LinearGradient(
+    colors: [Color(0xffda1421), Color(0xabda1421)],
+    stops: [0, 1],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  late var bleu = const Color(0xFF40C4FF);
+
+  late var bleuGradient = const LinearGradient(
+    colors: [Color(0xff40C4FF), Color(0xab40C4FF)],
+    stops: [0, 1],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:Ibet/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '/helpers/frontHelpers.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp,DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ibet Pronostics',
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).copyWith(
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: FrontHelpers().blanc)),
+      home: Home(),
+    );
+  }
+}
